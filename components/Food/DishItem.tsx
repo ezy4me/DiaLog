04afb67c140915ba5677137720caea1016480976
ezy4me <mrronal@mail.ru>
2024-02-13 23:00:17 +0300

    fix

--- components/Food/DishItem.tsx
+++ components/Food/DishItem.tsx
@@ -1,8 +1,18 @@
-import React from "react";
-import { Box, Text, HStack, VStack, useColorMode, Badge } from "native-base";
+import React, { useState } from "react";
+import {
+  Box,
+  Text,
+  HStack,
+  VStack,
+  useColorMode,
+  Badge,
+  Pressable,
+} from "native-base";
+import { DishModalForm } from "./DishModalForm";
 
 const DishItem = ({ item }: { item: any }) => {
   const { colorMode } = useColorMode();
+  const [isEditOpen, setIsEditOpen] = useState(false);
 
   const totalCalories = (item: any) => {
     const total = item.foodDishes.reduce(
@@ -19,72 +29,98 @@ const DishItem = ({ item }: { item: any }) => {
     );
   };
 
+  const closeModal = () => {
+    setIsEditOpen(false);
+  };
+
   return (
-    <Box
-      borderRadius={16}
-      w="100%"
-      bg={{
-        linearGradient: {
-          colors:
-            colorMode == "light"
-              ? ["white", "white"]
-              : ["blueGray.800", "blueGray.900"],
-          start: [0, 0],
-          end: [1, 1],
-        },
-      }}
-      px={2}
-      mb={2}>
-      <VStack space={2} py={2} px={2}>
-        <HStack justifyContent="space-between" alignItems={"center"}>
-          <VStack>
-            <Box borderRadius={4} mt={2} alignItems={"flex-start"}>
-              <Text
-                w={64}
-                borderRadius={16}
-                fontSize={"md"}
-                fontWeight={"semibold"}
-                textTransform={"uppercase"}>
-                {item.name}
-              </Text>
-            </Box>
-            <Text fontSize={"md"} fontWeight={"bold"}>
-              {totalWeight(item)} гр.
-            </Text>
-          </VStack>
-          <Box
-            borderRadius={100}
-            borderColor={"indigo.300"}
-            borderWidth={2}
-            p={2}
-            alignItems={"center"}
-            justifyContent={"center"}
-            w={20}
-            h={20}>
-            <Text
-              fontSize={"md"}
-              fontWeight={"bold"}
-              textTransform={"uppercase"}>
-              {totalCalories(item)}
-            </Text>
-            <Text textTransform={"uppercase"}>ккал</Text>
-          </Box>
-        </HStack>
-        <HStack w={64} flexWrap={"wrap"}>
-          {item.foodDishes.map((i: any) => (
-            <Badge
-              bg={"indigo.500"}
-              variant={"solid"}
-              mb={1}
-              mr={1}
+    <>
+      <Pressable borderRadius={16} onPress={() => setIsEditOpen(true)}>
+        {({ isHovered, isFocused, isPressed }) => {
+          return (
+            <Box
+              borderRadius={16}
+              w="100%"
+              bg={{
+                linearGradient: {
+                  colors:
+                    colorMode == "light"
+                      ? ["white", "white"]
+                      : ["blueGray.800", "blueGray.900"],
+                  start: [0, 0],
+                  end: [1, 1],
+                },
+              }}
+              style={{
+                transform: [
+                  {
+                    scale: isPressed ? 0.98 : 1,
+                  },
+                ],
+              }}
               px={2}
-              key={i.id}>
-              {i.food.name}
-            </Badge>
-          ))}
-        </HStack>
-      </VStack>
-    </Box>
+              mb={2}>
+              <VStack space={2} py={2} px={2}>
+                <HStack justifyContent="space-between" alignItems={"center"}>
+                  <VStack>
+                    <Box borderRadius={4} mt={2} alignItems={"flex-start"}>
+                      <Text
+                        w={64}
+                        borderRadius={16}
+                        fontSize={"md"}
+                        fontWeight={"semibold"}
+                        textTransform={"uppercase"}>
+                        {item.name}
+                      </Text>
+                    </Box>
+                    <Text fontSize={"md"} fontWeight={"bold"}>
+                      {totalWeight(item)} гр.
+                    </Text>
+                  </VStack>
+                  <Box
+                    borderRadius={100}
+                    borderColor={"indigo.300"}
+                    borderWidth={2}
+                    p={2}
+                    alignItems={"center"}
+                    justifyContent={"center"}
+                    w={20}
+                    h={20}>
+                    <Text
+                      fontSize={"md"}
+                      fontWeight={"bold"}
+                      textTransform={"uppercase"}>
+                      {totalCalories(item)}
+                    </Text>
+                    <Text textTransform={"uppercase"}>ккал</Text>
+                  </Box>
+                </HStack>
+                <HStack w={64} flexWrap={"wrap"}>
+                  {item.foodDishes.map((i: any) => (
+                    <Badge
+                      bg={"indigo.500"}
+                      variant={"solid"}
+                      mb={1}
+                      mr={1}
+                      px={2}
+                      key={i.id}>
+                      {i.food.name}
+                    </Badge>
+                  ))}
+                </HStack>
+              </VStack>
+            </Box>
+          );
+        }}
+      </Pressable>
+      {isEditOpen && (
+        <DishModalForm
+          data={item}
+          isModalVisible={isEditOpen}
+          onClose={closeModal}
+        />
+      )}
+    </>
   );
 };
 
---
+++ components/Food/DishModalForm.tsx
@@ -0,0 +1,118 @@
+import {
+  AntDesign,
+  Fontisto,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
+import {
+  Box,
+  Button,
+  Modal,
+  FormControl,
+  Input,
+  Text,
+  HStack,
+  KeyboardAvoidingView,
+  useColorMode,
+} from "native-base";
+import React, { useEffect, useState } from "react";
+import { Platform } from "react-native";
+
+export const DishModalForm = ({
+  label,
+  edit,
+  data,
+  isModalVisible,
+  onClose,
+}: {
+  label?: boolean;
+  edit?: boolean;
+  data?: any;
+  isModalVisible?: boolean;
+  onClose?: () => void;
+}) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const { colorMode } = useColorMode();
+
+  useEffect(() => {
+    if (isModalVisible) setModalVisible(true);
+  }, [isModalVisible]);
+
+  return (
+    <Box>
+      {/* <TouchableWithoutFeedback onPress={() => setModalVisible(false)}> */}
+      <Modal
+        isOpen={modalVisible}
+        onClose={() => {
+          setModalVisible(false);
+          onClose && onClose();
+        }}
+        avoidKeyboard={false}
+        bottom="0"
+        justifyContent="flex-end"
+        animationPreset="slide"
+        size="full">
+        <KeyboardAvoidingView
+          style={{ flex: 1, justifyContent: "flex-end", width: "100%" }}
+          behavior={Platform.OS === "android" ? "padding" : "position"}>
+          <Modal.Content>
+            <Modal.CloseButton
+              _icon={{ color: "light.100" }}
+              borderRadius={100}
+              bg={"green.500"}
+            />
+            <Modal.Header>
+              <HStack space={2} alignItems={"center"}>
+                <MaterialCommunityIcons
+                  name="food-apple"
+                  size={24}
+                  color={colorMode == "light" ? "black" : "white"}
+                />
+                <Text fontWeight={"semibold"}>{data.name}</Text>
+              </HStack>
+            </Modal.Header>
+            <Modal.Body>
+              <FormControl>
+                {data.foodDishes.map((item: any) => (
+                  <Box key={item.id}>
+                    <FormControl.Label my={2}>
+                      {item.food.name}
+                    </FormControl.Label>
+                    <Input
+                      value={item.weight.toString()}
+                      variant="rounded"
+                      keyboardType="numeric"
+                      placeholder="..."
+                      leftElement={
+                        <Box ml={4}>
+                          <Fontisto
+                            name="circle-o-notch"
+                            size={16}
+                            color={colorMode == "light" ? "black" : "white"}
+                          />
+                        </Box>
+                      }
+                      rightElement={
+                        <Box
+                          borderLeftWidth={1}
+                          borderLeftColor={"muted.200"}
+                          p={2}
+                          children={"g"}
+                        />
+                      }
+                    />
+                  </Box>
+                ))}
+              </FormControl>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button flex="1" borderRadius={32} bg={"green.500"}>
+                Сохранить
+              </Button>
+            </Modal.Footer>
+          </Modal.Content>
+        </KeyboardAvoidingView>
+      </Modal>
+      {/* </TouchableWithoutFeedback> */}
+    </Box>
+  );
+};
